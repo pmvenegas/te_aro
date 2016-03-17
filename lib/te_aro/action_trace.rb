b29cd5e18274cbc4e0e3412713703478ea6c5a03
Michael Fowler <michael@Michaels-MacBook-Pro.local>
2016-03-18 11:50:06 +1300

    TracePoint-ise trace facility
    Avoids invoking per-line, etc.

--- lib/te_aro/action_trace.rb
+++ lib/te_aro/action_trace.rb
@@ -10,34 +10,22 @@ module TeAro
     def start
       @calls = []
       @accumulator = { initial: {}, current: {} }
+      @file_p =  -> (path) { @blacklist.none? { |entry| path.match(entry) } }
 
-      set_trace_func proc { |event, file, line, id, binding, classname|
-        is_call = event == 'call'
-        is_ret  = event == 'return'
-        subject = binding.try!(:eval, 'self')
-        object_id = binding.try!(:eval, 'self.object_id')
+      @tracepoint = TracePoint.new(:call, :return) do |trace|
+        extract(trace, @calls) if @file_p.call(trace.path)
+      end
 
-        file_p =  -> (path) { @blacklist.none? { |entry| path.match(entry) } }
-
-        if is_call && file_p.call(file)
-          depth = caller.size
-          @calls << [event, file, line, id, classname, object_id, depth]
-          annotate(@accumulator, subject)
-        elsif is_ret && file_p.call(file)
-          depth = caller.size
-          @calls << [event, file, line, id, classname, object_id, depth]
-          annotate(@accumulator, subject)
-        end
-      }
+      @tracepoint.enable
     end
 
     def stop
-      set_trace_func nil
+      @tracepoint.disable
     end
 
     def print_trace
       calls.each do |event, file, _, id, classname, object_id, depth|
-        sigil = event == 'call' ? '->' : '<-'
+        sigil = event == :call ? '->' : '<-'
         @logger.info "#{'.'*depth}#{sigil}#{classname}:#{object_id}:#{id} from #{file}"
       end
       nil
@@ -62,6 +50,21 @@ module TeAro
     end
 
     private
+
+    def extract(tracepoint, calls)
+      event         = tracepoint.event
+      path          = tracepoint.path
+      line          = tracepoint.lineno
+      method_id     = tracepoint.method_id
+      defined_class = tracepoint.defined_class
+
+      subject = tracepoint.self
+      object_id = subject.object_id
+
+      depth = caller.size
+      calls << [event, path, line, method_id, defined_class, object_id, depth]
+      annotate(@accumulator, subject)
+    end
 
     def annotate(accumulator, object)
       return unless object && object.is_a?(ActiveRecord::Base)
